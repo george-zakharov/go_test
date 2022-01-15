d9a1f837c539dfc6eb0a59298312eb166ceb66f6
George Zakharov <george_zakharov@mail.ru>
2022-01-15 21:22:59 +0300

    New web app - final fixes

--- main.go
+++ main.go
@@ -51,6 +51,22 @@ type Search struct {
 	Results    Results
 }
 
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 	//w.Write([]byte("<h1>Hello World1111!</h1>")) // TODO: FOR TEST
@@ -104,6 +120,10 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	if ok := !search.IsLastPage(); ok {
+		search.NextPage++
+	}
+
 	err = tpl.Execute(w, search)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
